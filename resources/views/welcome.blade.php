10cb6f0b58d7a6246b02fdd24ead79c67401903b
RafaelEmery <rafael.emerycade@gmail.com>
2020-06-26 19:54:21 -0300

    Removing social icons

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -149,25 +149,6 @@
           </ul>
           <p class="text-muted small mb-4 mb-lg-0">Feito por &copy; <a href="http://rafaelemery.github.io">Rafael Emery</a> 2020.</p>
         </div>
-        <div class="col-lg-6 h-100 text-center text-lg-right my-auto">
-          <ul class="list-inline mb-0">
-            <li class="list-inline-item mr-3">
-              <a href="#">
-                <i class="fab fa-facebook fa-2x fa-fw"></i>
-              </a>
-            </li>
-            <li class="list-inline-item mr-3">
-              <a href="#">
-                <i class="fab fa-twitter-square fa-2x fa-fw"></i>
-              </a>
-            </li>
-            <li class="list-inline-item">
-              <a href="#">
-                <i class="fab fa-instagram fa-2x fa-fw"></i>
-              </a>
-            </li>
-          </ul>
-        </div>
       </div>
     </div>
   </footer>
