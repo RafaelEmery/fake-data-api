6b919efa9bc6438a114e52137b44c2c250fdbd54
RafaelEmery <rafael.emerycade@gmail.com>
2020-06-26 19:33:29 -0300

    Landing page done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,181 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="en">
 
-        <title>Laravel</title>
+<head>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+  <title>Fake Data API</title>
 
-            .full-height {
-                height: 100vh;
-            }
+  <!-- Bootstrap core CSS -->
+  <link href="{{asset('template/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+  <!-- Custom fonts for this template -->
+  <link href="{{asset('/template/vendor/fontawesome-free/css/all.min.css" rel="stylesheet')}}">
+  <link href="{{asset('/template/vendor/simple-line-icons/css/simple-line-icons.css')}}" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
 
-            .position-ref {
-                position: relative;
-            }
+  <!-- Custom styles for this template -->
+  <link href="{{asset('/template/css/landing-page.css')}}" rel="stylesheet">
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+</head>
 
-            .content {
-                text-align: center;
-            }
+<body>
 
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Teste no Heroku
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+  <!-- Masthead -->
+  <header class="masthead text-white text-center">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-9 mx-auto">
+          <h1 class="mb-5">API em Laravel feita para gerar dados falsos e aumentar a produtividade!</h1>
+        </div>
+        <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
+            <div class="row">
+                <div class="col-12 col-md-6" style="margin-left: 150px;">
+                <a type="button" target="_blank" href="http://rafaelemery-fake-data-api.herokuapp.com/docs/" class="btn btn-block btn-lg btn-primary" style="border-radius: 8px;">Leia a documentação</a>
                 </div>
             </div>
         </div>
-    </body>
+      </div>
+    </div>
+  </header>
+
+  <!-- Icons Grid -->
+  <section class="features-icons bg-light text-center">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4">
+          <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+            <div class="features-icons-icon d-flex">
+              <i class="icon-game-controller m-auto text-primary"></i>
+            </div>
+            <h3>Produtos</h3>
+            <p class="lead mb-0">Produtos de um e-commerce e com vários tipos de dados.</p>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+            <div class="features-icons-icon d-flex">
+              <i class="icon-notebook m-auto text-primary"></i>
+            </div>
+            <h3>Posts</h3>
+            <p class="lead mb-0">Posts simples de um blog com seus comentários.</p>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="features-icons-item mx-auto mb-0 mb-lg-3">
+            <div class="features-icons-icon d-flex">
+              <i class="icon-speech m-auto text-primary"></i>
+            </div>
+            <h3>Comentários</h3>
+            <p class="lead mb-0">Comentários simples relacionados com seu respectivo post.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Image Showcases -->
+  <section class="showcase">
+    <div class="container-fluid p-0">
+      <div class="row no-gutters">
+
+        <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('/template/img/bg-docs.png');"></div>
+        <div class="col-lg-6 order-lg-1 my-auto showcase-text">
+          <h2>Documentação</h2>
+          <p class="lead mb-0">
+              Gerada com o pacote <a target="_blank" href="https://github.com/mpociot/laravel-apidoc-generator">laravel-apidoc-generator</a> com o objetivo de facilitar o consumo e entendimento da API. 
+              Por enquanto, só possui o consumo da API por <i>bash</i> e <i>Javascript</i>.
+          </p>
+        </div>
+      </div>
+      <div class="row no-gutters">
+        <div class="col-lg-6 text-white showcase-img" style="background-image: url('/template/img/bg-rest.jpg');"></div>
+        <div class="col-lg-6 my-auto showcase-text">
+          <h2>Padrão REST</h2>
+          <p class="lead mb-0">
+              Feita com base no <i>Representational State Transfer</i> (REST), onde seguiremos o protocolo HTTP com quatro métodos básicos:
+              GET, POST, PUT, DELETE. Por enquanto, somente as requisições do tipo GET funcionam.
+          </p>
+        </div>
+      </div>
+      <div class="row no-gutters">
+        <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('/template/img/bg-json.png');"></div>
+        <div class="col-lg-6 order-lg-1 my-auto showcase-text">
+          <h2>Respostas em JSON</h2>
+          <p class="lead mb-0">
+                Os retornos para as requisições chegam em formato JSON (Javascript Object Notation), o que garante simplicidade para a API. Caso você use o Google Chrome, use a extensão
+                <a target="_blank" href="https://chrome.google.com/webstore/detail/json-viewer/gbmdgpbipfallnflgajpaliibnhdgobh?hl=pt-BR">JSON Viewer</a> para melhor visualização.
+          </p>
+        </div>
+      </div>
+      <div class="row no-gutters">
+        <div class="col-lg-6 text-white showcase-img" style="background-image: url('/template/img/bg-heroku.png');"></div>
+        <div class="col-lg-6 my-auto showcase-text">
+          <h2>Heroku + PostgreSQL</h2>
+          <p class="lead mb-0">
+              Hospedagem usando a plataforma <a target = "_blank" href="https://heroku.com">Heroku</a> para teste de aplicações em uso na Web.
+              Como banco de dados, foi usado o <a target="_blank" href="https://www.postgresql.org/">PostgreSQL</a> através de um <i>add-on</i> do Heroku chamado <i>Heroku Postgres</i>
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Footer -->
+  <footer class="footer bg-light">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 h-100 text-center text-lg-left my-auto">
+          <ul class="list-inline mb-2">
+            <li class="list-inline-item">
+                Gostaria de testar as respostas?
+            </li>
+            <li class="list-inline-item">
+              <a href="https://rafaelemery-fake-data-api.herokuapp.com/shop" target="_blank">Produtos</a>
+            </li>
+            <li class="list-inline-item">&sdot;</li>
+            <li class="list-inline-item">
+              <a href="https://rafaelemery-fake-data-api.herokuapp.com/blog/post" target="_blank">Posts</a>
+            </li>
+            <li class="list-inline-item">&sdot;</li>
+            <li class="list-inline-item">
+              <a href="https://rafaelemery-fake-data-api.herokuapp.com/blog/comment" target="_blank">Comentários</a>
+            </li>
+          </ul>
+          <p class="text-muted small mb-4 mb-lg-0">Feito por &copy; <a href="http://rafaelemery.github.io">Rafael Emery</a> 2020.</p>
+        </div>
+        <div class="col-lg-6 h-100 text-center text-lg-right my-auto">
+          <ul class="list-inline mb-0">
+            <li class="list-inline-item mr-3">
+              <a href="#">
+                <i class="fab fa-facebook fa-2x fa-fw"></i>
+              </a>
+            </li>
+            <li class="list-inline-item mr-3">
+              <a href="#">
+                <i class="fab fa-twitter-square fa-2x fa-fw"></i>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="#">
+                <i class="fab fa-instagram fa-2x fa-fw"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="{{asset('/template/vendor/jquery/jquery.min.js')}}"></script>
+  <script src="{{asset('/template/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+
+</body>
+
 </html>
