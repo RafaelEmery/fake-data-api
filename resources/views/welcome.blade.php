0e7756843bd5d074010eff8960dd4fb04911ebe8
RafaelEmery <rafael.emerycade@gmail.com>
2020-06-26 00:11:11 -0300

    Deploy test: Welcome's title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Teste no Heroku
                 </div>
 
                 <div class="links">
